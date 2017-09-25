c7979089397ed20c8f2ca66289c754737a34ddf4
Anton Stupnikov <anton_stupnikov@epam.com>
2017-09-25 16:22:31 +0400

    in progress

---
+++ src/com/java/module1/Division.java
@@ -0,0 +1,4 @@
+package com.java.module1;
+
+public class Division {
+}
--- src/com/java/module1/TestJUnit.java
+++ src/com/java/module1/TestJUnit.java
@@ -1,7 +1,16 @@
 package com.java.module1;
 
+import com.epam.tat.module4.Calculator;
+
 public class TestJUnit {
 
+    private Calculator calculator = new Calculator();
+
     public static void main(String[] args) {
+
+
+
+
+//        Тест с ожидаемым исключением: @Test(expected = NullPointerException.class, timeout)
     }
 }
