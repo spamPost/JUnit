62be1ae6efc76fa2c08e0ed6fd1a841b7ee34d66
Anton Stupnikov <anton_stupnikov@epam.com>
2017-09-25 17:08:27 +0400

    .

--- src/com/java/module1/TestJUnit.java
+++ src/com/java/module1/TestJUnit.java
@@ -1,6 +1,10 @@
 package com.java.module1;
 
 import com.epam.tat.module4.Calculator;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 public class TestJUnit {
 
@@ -8,8 +12,13 @@ public class TestJUnit {
 
     public static void main(String[] args) {
 
+        @Category(Unit.class)
+        @Test
+        public void testIsEmpty(){}
 
-
+//для запуска набора тестов
+//        @Suite.SuiteClasses({Subtraction.class})
+//        @RunWith(Suite.class)
 
 //        Тест с ожидаемым исключением: @Test(expected = NullPointerException.class, timeout)
     }
